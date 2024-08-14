b18cb8b3abd01c5f603387888096a639449f8f16
Softvence <softvence@Softvences-Mac-mini.local>
2024-08-14 17:20:46 +0600

    dash board done

--- lib/constants/text_font_style.dart
+++ lib/constants/text_font_style.dart
@@ -5,22 +5,14 @@ import 'package:google_fonts/google_fonts.dart';
 import '../gen/colors.gen.dart';
 
 class TextFontStyle {
-//Initialising Constractor
   TextFontStyle._();
-  //new
-  // static final headline50StyleMontserrat = GoogleFonts.montserrat(color: AppColors.cFFFFFF,fontSize: 50.sp, fontWeight: FontWeight.w400);
-  // static final headline27StyleMontserrat =
-  //     GoogleFonts.montserrat(color: AppColors.c0A0909, fontSize: 27.sp, fontWeight: FontWeight.w600);
 
   static final headline12w400C9E9E9EStyleInter = GoogleFonts.inter(
       color: AppColors.c9E9E9E, fontSize: 12.sp, fontWeight: FontWeight.w400);
-
   static final headline48w600CFFFFFFFFStylePoppins = GoogleFonts.poppins(
       color: AppColors.cffffff, fontSize: 48.sp, fontWeight: FontWeight.w600);
-
   static final headline16w600CFFFFFFFFStyleintere = GoogleFonts.inter(
       color: AppColors.cffffff, fontSize: 16.sp, fontWeight: FontWeight.w600);
-
   static final headline16w600C000000tyleiPoppins = GoogleFonts.poppins(
       color: AppColors.c000000, fontSize: 16.sp, fontWeight: FontWeight.w600);
   static final headline24w700C000000StyleInter = GoogleFonts.inter(
@@ -37,19 +29,6 @@ class TextFontStyle {
       color: AppColors.c000000, fontSize: 14.sp, fontWeight: FontWeight.w400);
   static final headline16w600C00000StyleInter = GoogleFonts.inter(
       color: AppColors.c000000, fontSize: 16.sp, fontWeight: FontWeight.w600);
-
-      static final headline24w700C000000StyleInter =
-      GoogleFonts.inter(color: AppColors.c000000, fontSize: 24.sp, fontWeight: FontWeight.w700);
-      static final headline16w700CffffffStyleInter =
-      GoogleFonts.inter(color: AppColors.cffffff, fontSize: 16.sp, fontWeight: FontWeight.w700);
-      static final headline16w400C848484StyleInter =
-      GoogleFonts.inter(color: AppColors.c848484, fontSize: 16.sp, fontWeight: FontWeight.w400);
-      static final headline18w500C848484StyleInter =
-      GoogleFonts.inter(color: AppColors.c848484, fontSize: 18.sp, fontWeight: FontWeight.w500);
-      static final headline14w400C848484StyleInter =
-      GoogleFonts.inter(color: AppColors.c3B5998, fontSize: 14.sp, fontWeight: FontWeight.w400);
-      static final headline18w700C22252DStyleInter =
-      GoogleFonts.inter(color: AppColors.c22252D, fontSize: 18.sp, fontWeight: FontWeight.w700);
-
-
+  static final headline18w700C22252DStyleInter = GoogleFonts.inter(
+      color: AppColors.c22252D, fontSize: 18.sp, fontWeight: FontWeight.w700);
 }
